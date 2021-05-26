54d01a710fc1f3d91f3b10d75803cc4dd5f94612
Aiman Aqari <aaqari@e2r6p8.1337.ma>
2021-05-26 11:23:20 +0100

    create the first list  of numbers in stack

--- main.c
+++ main.c
@@ -6,21 +6,43 @@
 /*   By: aaqari <aaqari@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 10:44:32 by aaqari            #+#    #+#             */
-/*   Updated: 2021/05/26 10:07:48 by aaqari           ###   ########.fr       */
+/*   Updated: 2021/05/26 11:22:55 by aaqari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
 
-int ct_list(t_list *node, int val)
+void push_front(t_list **node, int val)
 {
-	node->val = 1;
-	node->next = NULL;
-	printf("val inside fun %d\n", node->val);
-	return (node->val);
+	t_list *current;
+	
+	if ((*node) == NULL) {
+		(*node) = (t_list *)malloc(sizeof(t_list));
+		(*node)->val = val;
+		(*node)->next = NULL;
+		return ;
+	}
+
+	current = (*node);
+	while (current->next != NULL)
+		current = current->next;
+	current->next = (t_list *)malloc(sizeof(t_list));
+	current->next->val = val;
+	current->next->next = NULL;
+	
 }
 
+void print_list(t_list *node)
+{
+	t_list *current = node;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->val);
+		current = current->next;
+	}
+}
 
 int main(int argc, char *argv[])
 {
@@ -28,15 +50,15 @@ int main(int argc, char *argv[])
 		int value;
         t_list *node;
         
-        i = 0;
-		node = (t_list *)malloc(sizeof(t_list));
-		if (node == NULL)
-			return (1);
+        i = 1;
+		node = NULL;
         while (i < argc)
         {
 			value = ft_atoi(argv[i]);
-            ct_list(node, value);
+			push_front(&node, value);
             i++;
         }
+		print_list(node);
+		free(node);
     return (0);
 }
