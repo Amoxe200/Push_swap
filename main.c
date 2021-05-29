5dd0f69ef795b84066b37903199ac50261c2d4a3
Amoxe200 <aimanaqari@gmail.com>
2021-05-29 15:29:45 +0100

    Another Testing Commit

--- main.c
+++ main.c
@@ -6,14 +6,14 @@
 /*   By: aaqari <aaqari@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 10:44:32 by aaqari            #+#    #+#             */
-/*   Updated: 2021/05/26 11:22:55 by aaqari           ###   ########.fr       */
+/*   Updated: 2021/05/29 15:27:33 by aaqari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
 
-void push_front(t_list **node, int val)
+void push_last(t_list **node, int val)
 {
 	t_list *current;
 	
@@ -44,18 +44,26 @@ void print_list(t_list *node)
 	}
 }
 
+void sa(t_list *node)
+{
+	
+}
+
 int main(int argc, char *argv[])
 {
         int i;
 		int value;
         t_list *node;
+        t_list *node_b;
+        
         
         i = 1;
 		node = NULL;
+		node_b = NULL;
         while (i < argc)
         {
 			value = ft_atoi(argv[i]);
-			push_front(&node, value);
+			push_last(&node, value);
             i++;
         }
 		print_list(node);
