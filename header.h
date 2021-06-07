e3b872839f6d91f89e892918b23fbc568f0ba33a
Aiman <aimanaqari@gmail.com>
2021-06-07 11:58:31 +0100

    Fixing Some Bugs

--- header.h
+++ header.h
@@ -6,7 +6,7 @@
 /*   By: amoxe <amoxe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 10:44:39 by aaqari            #+#    #+#             */
-/*   Updated: 2021/06/06 17:58:38 by amoxe            ###   ########.fr       */
+/*   Updated: 2021/06/07 11:49:56 by amoxe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,10 +37,9 @@ void rra(t_list **nodea);
 void twNum(t_list **node);
 void reversb(t_list **nodeb);
 void rrr(t_list **nodea , t_list **nodeb);
-void checkNumbers(t_list **node, t_list **nb, int num);
+void checkNumbers(t_list **node, t_list **nb,int num);
 void sortTnum(t_list **node);
 void cc(t_list **hd, t_list **md, t_list **lt, t_list **nd);
 void caseone(t_list **h, t_list **m, t_list **l);
 void checkCase(int hv, int mv, int lv, t_list **nd);
-void check_args(int num, t_list **na, t_list **nb);
 #endif
